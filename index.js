ce27880faea12d0e8eaec5e41e2560b66987c4fb
Michael Jester <m.jester.93@gmail.com>
2020-09-22 17:00:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+const findMinAndRemoveSorted = (array) => {
+  return array.shift()
+}
 
+const merge = (firstSubarray, secondSubArray) => {
+  let sortedArray = [];
+
+  while ( firstSubarray.length !== 0 && secondSubArray.length !== 0 ) {
+    if (firstSubarray[0] < secondSubArray[0]) {
+      sortedArray.push(findMinAndRemoveSorted(firstSubarray))
+    } else {
+      sortedArray.push(findMinAndRemoveSorted(secondSubArray))
+    }
+  }
+
+  return sortedArray.concat(firstSubarray).concat(secondSubArray)
+}
+
+const mergeSort = (array) => {
+  let midpoint = array.length / 2;
+  let firstSubarray = array.slice(0, midpoint);
+  let secondSubArray = array.slice(midpoint, array.length);
+
+  return array.length > 1 ? merge(mergeSort(firstSubarray), mergeSort(secondSubArray)) : array
+}
