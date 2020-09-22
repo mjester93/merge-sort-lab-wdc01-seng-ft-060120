9c397527320205819f2b3456d614543ea80f4c36
Michael Jester <m.jester.93@gmail.com>
2020-09-22 16:48:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const findMinAndRemoveSorted = (array) => {
   return array.shift()
 }
+
+const merge = (firstSubarray, secondSubArray) => {
+  let minOne = firstSubarray[0];
+  let minTwo = firstSubarray[1];
+}
