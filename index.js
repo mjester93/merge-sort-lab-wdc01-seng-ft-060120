b4015d128035eb50b14847f81d0ed0fccb6abbc7
Michael Jester <m.jester.93@gmail.com>
2020-09-22 16:53:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const merge = (firstSubarray, secondSubArray) => {
   let sortedArray = [];
 
   while ( firstSubarray.length !== 0 && secondSubArray.length !== 0 ) {
-    if (firstSubarray[0]) < secondSubArray[0]) {
+    if (firstSubarray[0] < secondSubArray[0]) {
       sortedArray.push(findMinAndRemoveSorted(firstSubarray))
     } else {
       sortedArray.push(findMinAndRemoveSorted(secondSubArray))
